4298a46620b25a670be36ceebd6ce6d80a29b505
cdiaz2024 <cdiaz2024@communityschoolnaples.org>
2023-11-12 20:04:49 -0500

    Figma

---
+++ src/AppNew.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import './App.css';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import {NoteNavBar, NoteCollection} from "./ui-components";
+class App extends Component {
+  render() {
+  return (
+    <div className="App"><header className="App-header">
+        <NoteNavBar/><NoteCollection/>
+      </header></div>
+    );
+}
+}
+
+export default withAuthenticator(App);
+
